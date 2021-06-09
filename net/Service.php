47727b122672828f7bafc9c34a90d61a1618c0b6
lak5566 <like55667642@gmail.com>
2021-06-09 15:01:47 +0800

    聯絡我

--- net/Service.php
+++ net/Service.php
@@ -14,58 +14,52 @@
         </style>
     </head>
 
-    <body onload="init()">
+    <body>
         <div class="mail">
             <div class="container">
                 <div class="header" style="text-align:center; font-size:24px; padding:30px 0">
                 聯絡我
                 </div>
-                <form id="msg" name="msg" enctype="text/plain" accept-charset="utf-8">
+                <form id="msg" name="msg" method="post" action="" >
                     <div class="form-group">
                         <label for="nickname">暱稱</label>
-                        <input type="text" class="form-control" id="nameText" name="nickname" placeholder="輸入您的暱稱" required >
+                        <input type="text" class="form-control" id="nickname" name="nickname" placeholder="輸入您的暱稱" required >
                     </div>
                     <div class="form-group">
                         <label for="email">電子郵件信箱</label>
-                        <input type="email" class="form-control" id="emailText" name="email" placeholder="輸入E-mail" required>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="輸入E-mail" required>
                     </div>
                     <div class="form-group">
                         <label for="subject">信件主旨</label>
-                        <input type="text" class="form-control" id="subText" name="subject" placeholder="輸入主旨" required>
+                        <input type="text" class="form-control" id="subject" name="subject" placeholder="輸入主旨" required>
                     </div>
                     <div class="form-group">
                         <label for="content">信件內容</label>
-                        <textarea class="form-control" id="bodyText" name="content" rows="5"placeholder="輸入訊息" required></textarea>
+                        <textarea class="form-control" id="content" name="content" rows="5"placeholder="輸入訊息" required></textarea>
                     </div>
                     <div align="right">
                         <button type="submit" class="btn btn-default" button onclick="submitHandler()">寄出</button>
                     </div>
                     
                 </form>
-                <script>  
-                    function submitHandler()
+                <?php 
+                    if(@$_POST['nickname']!='')
                     {
-                        var to = "40743254@gm.nfu.edu.tw";//輸入要接收信件的信箱
-                        var name = nameText.value;
-                        var email = emailText.value;
-                        var subject = subText.value;
- 
-                        var body = ""+bodyText.value+'%0A%0A%0A';
-                            body += "From："+nameText.value+'%0A';
-                            body += "Email："+emailText.value+'%0A';
-                            body += "Tel："+telText.value;
+                        $nickname=$_POST['nickname'];
+                        $email=$_POST['email'];
+                        $sub=$_POST['subject'];
+                        $content=$_POST['content'];
 
-                        mailTo.href="mailto:"+to+"?subject="+subject+"&body="+body;
-                        mailTo.click();
+                        mb_internal_encoding("utf-8");
+                        $to="40743254@gmail.com";//填入自己的電子信箱
+                        $subject=mb_encode_mimeheader("$sub","utf-8");
+                        $message="$content";
+                        $headers="MIME-Version: 1.0\r\n";
+                        $headers.="Content-type: text/html; charset=utf-8\r\n";
+                        $headers.="From:".mb_encode_mimeheader("$nickname","utf-8")."<$email>\r\n";
+                        mail($to,$subject,$message,$headers);
                     }
-
-                    function init()
-                    {
-                        subText.value=initSubject;
-                        toText.value=initTo;
-                        bodyText.value=initBody;
-                    }
-                </script>
+                ?>
             </div>
         </div>
     </body>
