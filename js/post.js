3cf35a02eb6a64bda74ee8b3654b6dfb2b475a6d
herry2356 <herry2356@gmail.com>
2021-06-15 00:39:06 +0800

    last

--- js/post.js
+++ js/post.js
@@ -1,1 +1,12 @@
-document.getElementById('theForm').action = 'post.php';
+function processFormData() {
+    var itemData = document.getElementsByName("item").value;
+    var quantityData = document.getElementsByName("quantity").value;
+    var addressData = document.getElementsByName("address").value;
+    var wantData = document.getElementsByName("want").value;
+    var dateData = document.getElementsByName("date").value;
+
+}
+
+function formSubmit() {
+    document.getElementById("myform").submit();
+}
